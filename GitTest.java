7be347e6ddf297345121dcac28a5a563bf4b59cb
HDBingham <hdbingham@my.okcu.edu>
2016-03-24 19:41:02 -0400

    Update GitTest.java

--- GitTest.java
+++ GitTest.java
@@ -1,6 +1,7 @@
 public class GitTest {
 	
 	//adding to the code...
+	//This is Hunter Editing A Code uploaded by Matt
 	
 	public static void main(String[] args){
 		System.out.println("GITHUB Test!");
