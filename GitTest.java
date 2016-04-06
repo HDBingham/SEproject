86ec9de76e52773aaaa020e01f451608670ef877
MDNelson16 <mdnelson@my.okcu.edu>
2016-04-05 19:05:45 -0500

    Added additional comment
    Checking if comment will sync with local folder

--- GitTest.java
+++ GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
 	
 	//adding to the code...
 	//This is Hunter Editing A Code uploaded by Matt
+	//checking this
 	
 	public static void main(String[] args){
 		System.out.println("GITHUB Test!");
